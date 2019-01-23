112215b4313143efe8241eb479a84b44adeb883d
user149121 <user149121@la214.local>
2019-01-23 09:00:31 -0800

    Initial Commit

---
+++ MessageManager/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MessageManager
+//
+//  Created by user149121 on 1/23/19.
+//  Copyright © 2019 kilostudio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
